ae496092d965a8ea8c9431a47ecc4dc524532d27
breeze4 <ambiturner0@gmail.com>
2016-02-20 14:14:55 -0800

    configured c3p0

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -1,8 +1,10 @@
 package config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
 
 import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,6 +16,8 @@ public class DatabaseConfig {
     private final String dbUser;
     private final String dbPassword;
     private final String dbJdbcUrl;
+
+    private ComboPooledDataSource dataSource;
 
     public DatabaseConfig(Properties props) {
         this(props.getProperty("dbJdbcUrl"),
@@ -28,20 +32,34 @@ public class DatabaseConfig {
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
 
-        // TODO: add c3p0 for connection pooling
-        Connection conn = null;
         try {
-            conn = DriverManager.getConnection(dbUrl);
-            // Do something with the Connection
+            dataSource = new ComboPooledDataSource();
+            dataSource.setDriverClass("com.mysql.jdbc.Driver");
+            dataSource.setJdbcUrl(this.dbJdbcUrl);
+            dataSource.setUser(this.dbUser);
+            dataSource.setPassword(this.dbPassword);
             System.out.println("Connection established to: " +
-                    "database: " + conn.getMetaData().getDatabaseProductName() +
-                    " version: " + conn.getMetaData().getDatabaseProductVersion());
-            conn.close();
-        } catch (SQLException ex) {
-            // handle any errors
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
+                    "database: " + dataSource.getConnection().getMetaData().getDatabaseProductName() +
+                    " version: " + dataSource.getConnection().getMetaData().getDatabaseProductVersion());
+        } catch (Exception e) {
+            e.printStackTrace();
+            dataSource = null;
         }
+//        Connection conn = null;
+//        try {
+//
+//
+//            conn = DriverManager.getConnection(dbUrl);
+//            // Do something with the Connection
+//            System.out.println("Connection established to: " +
+//                    "database: " + conn.getMetaData().getDatabaseProductName() +
+//                    " version: " + conn.getMetaData().getDatabaseProductVersion());
+//            conn.close();
+//        } catch (SQLException ex) {
+//            // handle any errors
+//            System.out.println("SQLException: " + ex.getMessage());
+//            System.out.println("SQLState: " + ex.getSQLState());
+//            System.out.println("VendorError: " + ex.getErrorCode());
+//        }
     }
 }
