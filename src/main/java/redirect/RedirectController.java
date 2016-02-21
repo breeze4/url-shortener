ea4ee29c145cb4d43bc8f52e5389711f798db71a
breeze4 <ambiturner0@gmail.com>
2016-02-20 21:58:30 -0800

    added logging for endpoints

--- src/main/java/redirect/RedirectController.java
+++ src/main/java/redirect/RedirectController.java
@@ -1,17 +1,21 @@
 package redirect;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import shorten.ShortenService;
 import spark.Spark;
 
 public class RedirectController {
 
     public static final String BASE_REDIRECT_URL = "/s";
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedirectController.class);
 
     public RedirectController(ShortenService service) {
 
         Spark.get(BASE_REDIRECT_URL + "/:shortenedUrl", (req, res) -> {
             String shortenedUrl = req.params(":shortenedUrl");
             String originalUrl = service.redirect(shortenedUrl);
+            LOGGER.info("Handling redirect: {}, shortened as: {}", originalUrl, shortenedUrl);
 
             res.status(302);
             res.header("Location", originalUrl);
--- src/main/java/shorten/ShortenController.java
+++ src/main/java/shorten/ShortenController.java
@@ -1,11 +1,14 @@
 package shorten;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Spark;
 
 public class ShortenController {
 
     public static final String BASE_SHORTEN_URL = "/shorten";
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShortenController.class);
     private final ShortenService service;
     private final ObjectMapper om;
 
@@ -20,6 +23,7 @@ public class ShortenController {
             ShortenRequest request = om.readerFor(ShortenRequest.class).readValue(body);
             String originalUrl = request.getOriginalUrl();
             String shortenedUrl = service.shorten(originalUrl);
+            LOGGER.info("Adding a shortened URL: {}, from: {}", shortenedUrl, originalUrl);
 
             ShortenResponse response = new ShortenResponse(originalUrl, shortenedUrl);
             res.header("Content-Type", "application/json;charset=utf-8");
